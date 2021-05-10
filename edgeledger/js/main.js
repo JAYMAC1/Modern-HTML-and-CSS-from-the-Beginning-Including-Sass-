aaf2bb50950c077361b3159feb989cd090541b95
James McNeil <mcneil_james@hotmail.com>
2021-05-10 18:13:57 +0100

    styled form and javascript mapp

---
+++ edgeledger/js/main.js
@@ -0,0 +1,9 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 55.43764685549499, lng: -4.935076113262928 },
+    zoom: 9,
+  });
+}
+// initMap();
